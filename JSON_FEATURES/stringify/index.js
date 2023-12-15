0030e441287600e6cd389141fb016e15d59c51a1
mounika.neerati <mounika.neerati93@gmail.com>
2023-12-15 11:53:04 +0530

    third commit

---
+++ JSON_FEATURES/stringify/index.js
@@ -0,0 +1,15 @@
+var data={
+    name:'Mounika',
+    age:29
+
+}
+
+var json = JSON.stringify(data);
+console.log(data)
+
+
+//date 
+//value()
+
+var date = new Date();
+console.log(date.valueOf())
